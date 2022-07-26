9e2872db784b434a832194519d050e5fc75e049b
zaccowan <68406370+zaccowan@users.noreply.github.com>
2022-07-25 21:35:07 -0400

    Navbar Setup

---
+++ components/Navbar.jsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import { SearchIcon, ShoppingBagIcon } from "@heroicons/react/outline";
+
+function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-apple-gray">
+      <ul className="flex items-center justify-evenly mx-auto max-w-6xl">
+        <Link href="/">
+          <a className="navItem ">
+            <img src="https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__b5er5ngrzxqq_large.svg" />
+          </a>
+        </Link>
+
+        <Link href="/store">
+          <a className="navItem">Store</a>
+        </Link>
+
+        <Link href="/mac">
+          <a className="navItem">Mac</a>
+        </Link>
+
+        <Link href="/ipad">
+          <a className="navItem">iPad</a>
+        </Link>
+
+        <Link href="/iphone">
+          <a className="navItem">iPhone</a>
+        </Link>
+
+        <Link href="/watch">
+          <a className="navItem">Watch</a>
+        </Link>
+
+        <Link href="/airpods">
+          <a className="navItem">AirPods</a>
+        </Link>
+
+        <Link href="/tv-home">
+          <a className="navItem">TV & Home</a>
+        </Link>
+
+        <Link href="/services">
+          <a className="navItem">Only on Apple</a>
+        </Link>
+
+        <Link href="/shop/accessories/all">
+          <a className="navItem">Accessories</a>
+        </Link>
+
+        <Link href="/support">
+          <a className="navItem">Support</a>
+        </Link>
+        <button>
+          <SearchIcon className="navItem w-4" />
+        </button>
+        <button>
+          <ShoppingBagIcon className="navItem w-4" />
+        </button>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,54 @@
+import Link from "next/link";
+import { ArrowSmUpIcon, ChevronRightIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [windowWidth, setWindowWidth] = useState(null);
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
   return (
-    <div>
-      <h1 className="text-red-400">Howdy</h1>
-    </div>
+    <main className="mt-10 bg-gray-100">
+      <section className="py-5 flex items-center justify-center space-x-4">
+        <img
+          src="https://www.apple.com/v/home/aq/images/logos/unicef-relief-fund/unicef_logo_dark__ejvlglygb3ee_large.png"
+          alt=""
+        />
+        <Link href="https://www.apple.com/v/home/aq/images/logos/unicef-relief-fund/unicef_logo_dark__ejvlglygb3ee_large.png">
+          <div className="flex items-center justify-center">
+            <a className="blueLink text-sm">
+              Donate to support families affected by the war in Ukraine
+            </a>
+            <ArrowSmUpIcon className="blueLink w-6 rotate-45" />
+          </div>
+        </Link>
+      </section>
+
+      <section>
+        <div className="relative h-[736px]">
+          <div className="w-1/2 h-full absolute top-0 bottom-0 left-[25%] z-10 flex flex-col items-center justify-center gap-4">
+            <h2 className="text-6xl font-semibold">MacBook Air</h2>
+            <h3 className="text-3xl">Supercharged by M2</h3>
+            <div className="flex items-center justify-center gap-8">
+              <Link href="https://www.apple.com/v/home/aq/images/logos/unicef-relief-fund/unicef_logo_dark__ejvlglygb3ee_large.png">
+                <div className="flex items-center justify-center">
+                  <a className="blueLink text-xl">Learn more</a>
+                  <ChevronRightIcon className="blueLink w-6 " />
+                </div>
+              </Link>
+              <Link href="https://www.apple.com/v/home/aq/images/logos/unicef-relief-fund/unicef_logo_dark__ejvlglygb3ee_large.png">
+                <div className="flex items-center justify-center">
+                  <a className="blueLink text-xl">Buy</a>
+                  <ChevronRightIcon className="blueLink w-6 " />
+                </div>
+              </Link>
+            </div>
+          </div>
+          <div className="block lg:absolute top-0 left-0 right-0 h-full w-full">
+            <figure className="bg-[url('https://www.apple.com/v/home/aq/images/heroes/macbook-air/hero_macbookair__el7scava26mq_largetall.png')] w-full h-full"></figure>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
