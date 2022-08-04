d6cd460e0c37ae8d98bf036c9a5df4d2c9986e75
zaccowan <68406370+zaccowan@users.noreply.github.com>
2022-08-04 14:58:34 -0400

    Navbar Responsiveness Changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,30 @@
 import Link from "next/link";
-import { SearchIcon, ShoppingBagIcon } from "@heroicons/react/outline";
+import {
+  SearchIcon,
+  ShoppingBagIcon,
+  MenuIcon,
+  MenuAlt4Icon,
+} from "@heroicons/react/outline";
+import Image from "next/image";
 
 function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-black/70 backdrop-blur-sm">
-      <ul className="flex items-center justify-evenly mx-auto max-w-6xl">
-        <Link href="/">
-          <a className="navItem ">
-            <img src="https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__b5er5ngrzxqq_large.svg" />
+      <ul className="flex flex-1 items-center justify-between mx-8 lg:justify-evenly lg:mx-auto max-w-6xl">
+        <button className="block lg:hidden">
+          <MenuAlt4Icon className="navItem w-8 block" />
+        </button>
+        <Link href="https://zacbox.vercel.app">
+          <a
+            src="https://zacbox.vercel.app"
+            className="navItem block mx-auto lg:mx-0"
+          >
+            <Image
+              src="https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__b5er5ngrzxqq_large.svg"
+              height={50}
+              width={50}
+              alt=""
+            />
           </a>
         </Link>
 
@@ -54,7 +71,7 @@ function Navbar() {
           <SearchIcon className="navItem w-4" />
         </button>
         <button>
-          <ShoppingBagIcon className="navItem w-4" />
+          <ShoppingBagIcon className="navItem w-4 block" />
         </button>
       </ul>
     </nav>
